14b022dcb1df973d5df532d9b208497d71579cff
hawks900@gmail.com <hawks900@gmail.com@e46abff3-1524-1326-238b-bf475d51c56e>
2015-02-19 04:03:18 +0000

    My Final code along with generated JavaDoc

--- Project1/src/com/testing/project1/Main.java
+++ Project1/src/com/testing/project1/Main.java
@@ -3,17 +3,37 @@ package com.testing.project1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		if(FileUtility.isFilePresent(args[0])){
+
+		if (FileUtility.isFilePresent(args[0])) {
 			String[] lines = FileUtility.getLines();
-			
-			//We should resize array to minimize wastage
-			for(String eachLine : lines){
-				System.out.println(eachLine);
+
+			int lineNumber = 1;
+			// We should resize array to minimize space waste
+			for (String eachLine : lines) {
+				// get the duplicate words per each line
+				String[] dublicateWords = FileUtility
+						.getDublicateWords(eachLine);
+				if (dublicateWords.length > 0) {
+					printOutPutToUser(lineNumber, dublicateWords);
+				}
+				lineNumber++;
 			}
 		}
 
 	}
 
+	/**
+	 * Print the value to the user with the format specified in the sample
+	 * output file
+	 * 
+	 * @param lineNumber
+	 * @param dublicates
+	 */
+	static void printOutPutToUser(Integer lineNumber, String[] dublicates) {
+		System.out.print("Repeated word on line " + lineNumber + ": ");
+		for (String string : dublicates) {
+			System.out.print(string + " ");
+		}
+		System.out.print("\n");
+	}
 }
