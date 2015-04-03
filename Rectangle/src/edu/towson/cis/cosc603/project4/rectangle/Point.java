39413c1dfb55aa0638af34098d97784872009470
hawks900@gmail.com <hawks900@gmail.com@e46abff3-1524-1326-238b-bf475d51c56e>
2015-04-03 21:56:10 +0000

    I made Double as private class and used get and set methods for X and Y

--- Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Point.java
+++ Rectangle/src/edu/towson/cis/cosc603/project4/rectangle/Point.java
@@ -6,7 +6,7 @@ package edu.towson.cis.cosc603.project4.rectangle;
 public class Point {
 
 	/** x and y coordinates. */
-	Double x, y;
+	private Double x, y;
 
 	/**
 	 * Instantiates a new point.
@@ -17,7 +17,23 @@ public class Point {
 	 *            the y
 	 */
 	Point(Double x, Double y) {
-		this.x = x;/**Constructor assignment of the x should be x but it was y and that was our bug here  */
+		this.setX(x);/**Constructor assignment of the x should be x but it was y and that was our bug here  */
+		this.setY(y);
+	}
+
+	public Double getX() {
+		return x;
+	}
+
+	public void setX(Double x) {
+		this.x = x;
+	}
+
+	public Double getY() {
+		return y;
+	}
+
+	public void setY(Double y) {
 		this.y = y;
 	}
 }
