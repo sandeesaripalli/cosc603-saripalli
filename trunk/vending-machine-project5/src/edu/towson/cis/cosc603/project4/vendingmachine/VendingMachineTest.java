dff8d00affc41268450b696cd91d52b4fb3ea918
hawks900@gmail.com <hawks900@gmail.com@e46abff3-1524-1326-238b-bf475d51c56e>
2015-04-11 03:07:51 +0000

    Removed unused imports

--- trunk/vending-machine-project5/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
+++ trunk/vending-machine-project5/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
@@ -4,7 +4,6 @@
 package edu.towson.cis.cosc603.project4.vendingmachine;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
